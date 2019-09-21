98ff0b8e577cef861be7111e9e32243f4c48e6d1
liuyao <reactjser@163.com>
2019-09-21 13:24:09 +0800

    冒泡排序

---
+++ playWithAlgorithms/bubbleSort.js
@@ -0,0 +1,17 @@
+const SortTestHelper = require('./utils/sortTestHelper');
+const sortTestHelper = new SortTestHelper();
+
+// 选择排序
+const bubbleSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+};
+
+const n = 100000;
+const a = sortTestHelper.generateRandomArray(n, 0, n);
+sortTestHelper.testSort('Buttle Sort', bubbleSort, a);

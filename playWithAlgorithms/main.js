8ed4379ffdb5b680a0a70e9922fa627ff5641aa6
liuyao <reactjser@163.com>
2019-09-22 11:42:07 +0800

    test性能

--- playWithAlgorithms/main.js
+++ playWithAlgorithms/main.js
@@ -1,12 +1,20 @@
 const SortTestHelper = require('./utils/sortTestHelper');
+const bubbleSort = require('./bubbleSort');
+const selectionSort = require('./selectionSort');
 const insertionSort = require('./insertionSort');
 const mergeSort = require('./mergeSort');
 const sortTestHelper = new SortTestHelper();
 
-const n = 100000;
+const n = 10000;
 const swapTimes = 0;
-const arr1 = sortTestHelper.generateRandomArray(n, 0, n);
-// const arr1 = sortTestHelper.generateNearlyOrderedArray(n, swapTimes);
-const arr2 = sortTestHelper.copyIntArray(arr1);
-sortTestHelper.testSort('Insertion Sort', insertionSort, arr1);
-sortTestHelper.testSort('Merge Sort', mergeSort, arr2);
+const arr = sortTestHelper.generateRandomArray(n, 0, n);
+// const arr = sortTestHelper.generateNearlyOrderedArray(n, swapTimes);
+const arr1 = sortTestHelper.copyIntArray(arr);
+const arr2 = sortTestHelper.copyIntArray(arr);
+const arr3 = sortTestHelper.copyIntArray(arr);
+const arr4 = sortTestHelper.copyIntArray(arr);
+sortTestHelper.testSort('Sort', arr => arr.sort((a, b) => a - b), arr);
+sortTestHelper.testSort('Bubble Sort', bubbleSort, arr1);
+sortTestHelper.testSort('Selection Sort', selectionSort, arr2);
+sortTestHelper.testSort('Insertion Sort', insertionSort, arr3);
+sortTestHelper.testSort('Merge Sort', mergeSort, arr4);

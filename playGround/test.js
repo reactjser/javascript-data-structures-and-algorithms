03426020828f2d15dadb01653fbd3b5b7ba29d5d
liuyao <reactjser@163.com>
2019-09-27 10:14:46 +0800

    测试算法

--- playGround/test.js
+++ playGround/test.js
@@ -1,20 +1,30 @@
 const ListNode = require('./ListNode');
 
-var removeElements = function(head, val) {
-  const dummyHead = new ListNode(-1);
-  dummyHead.next = head;
+var mergeTwoLists = function(l1, l2) {
+  let l1Cur = l1;
+  let l2Cur = l2;
+  let dummyHead = new ListNode();
   let prev = dummyHead;
-  while (prev.next != null) {
-    if (prev.next.val === val) {
-      prev.next = prev.next.next;
+  while (l1Cur !== null || l2Cur !== null) {
+    if (!l1Cur) {
+      prev.next = new ListNode(l2Cur.val);
+      l2Cur = l2Cur.next;
+    } else if (!l2Cur) {
+      prev.next = new ListNode(l1Cur.val);
+      l1Cur = l1Cur.next;
+    } else if (l1Cur.val <= l2Cur.val) {
+      prev.next = new ListNode(l1Cur.val);
+      l1Cur = l1Cur.next;
     } else {
-      prev = prev.next;
+      prev.next = new ListNode(l2Cur.val);
+      l2Cur = l2Cur.next;
     }
+    prev = prev.next;
   }
-
   return dummyHead.next;
 };
 
-const head = new ListNode([1, 2, 6, 3, 4, 5, 6]);
-const result = removeElements(head, 6);
+const l1 = new ListNode([1, 2, 4]);
+const l2 = new ListNode([1, 3, 4]);
+const result = mergeTwoLists(l1, l2);
 console.log(result.toString());
